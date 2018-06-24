6bf1d0c9b361f774c7072084b50058e68febd8e0
Hamish-H <hamish.h@outlook.com>
2018-06-24 21:04:39 +0100

    updated app.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const newItemform = document.querySelector('#new-item-form');
+  newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+})
