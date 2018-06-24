12227c8c7a1547a9ac282416ed9abdfce86d6c51
Hamish-H <hamish.h@outlook.com>
2018-06-24 21:18:26 +0100

    updated js.app - delete all

--- js/app.js
+++ js/app.js
@@ -3,4 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
 
+  const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
 })
